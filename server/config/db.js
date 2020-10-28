c5213331091573b77354a74911f1751f306109f5
hanhan9334 <lhtony9334@126.com>
2020-10-28 11:23:39 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Han:han@cluster0.q9jmp.mongodb.net/mid-2020?retryWrites=true&w=majority"
+};
---
+++ server/routes/books.js
@@ -0,0 +1,121 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let Book = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  Book.find((err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', (req, res, next) => {
+  res.render('books/add', {
+    title: 'books', books: {
+      title: '',
+      description: '',
+      price: '',
+      author: '',
+      genre: ''
+    }
+  });
+
+
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/add', async (req, res, next) => {
+  const book = await new Book(req.body);
+  console.log(book);
+
+  try {
+    await book.save();
+    res.redirect('/books');
+  } catch (e) {
+    console.log(e);
+    res.end();
+  }
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', async (req, res, next) => {
+  const _id = req.params.id;
+
+  try {
+    const books = await Book.findById(_id);
+    if (!books) {
+      return res.status(404).send('Book not found.');
+    }
+    res.render('books/details', { title: 'Edit Book', books });
+  } catch (e) {
+    res.status(500).send();
+  }
+
+
+
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', async (req, res, next) => {
+
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+  const _id = req.params.id;
+  try {
+    const book = await Book.findByIdAndUpdate(_id, {
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      author: req.body.author,
+      genre: req.body.genre
+    })
+
+    res.redirect('/books');
+  } catch (e) {
+    console.log(e);
+    res.end();
+  }
+
+});
+
+// GET - process the delete by user id
+router.post('/delete/:id', async (req, res, next) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    res.redirect('/books')
+  } catch (e) {
+    console.log(e);
+    res.end();
+  }
+
+
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+});
+
+
+module.exports = router;
